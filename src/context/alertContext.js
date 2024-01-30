1e0d5876e02925fbc6dea5d5fa5efe380502017b
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-01-30 21:13:47 +0200

    remove some issues

--- src/context/alertContext.js
+++ src/context/alertContext.js
@@ -16,7 +16,7 @@ export const AlertProvider = ({ children }) => {
       value={{
         ...state,
         onOpen: (type, message) => setState({ isOpen: true, type, message }),
-        onClose: () => setState({ isOpen: false, type: '', message: '' }), //type: 'error', message: 'Something went wrong, please try again later!'
+        onClose: () => setState({ isOpen: false, type: '', message: '' }),
       }}
     >
       {children}
